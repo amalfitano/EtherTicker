eebd9c99765bf9878a1f23d51269344adce611d9
michaelamalfitano <amalfitano@umail.ucsb.edu>
2017-07-01 21:24:41 -0700

    My initial project setup

---
+++ EtherTicker1/EtherTicker1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EtherTicker1
+//
+//  Created by Michael Amalfitano on 7/1/17.
+//  Copyright © 2017 Michael Amalfitano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
